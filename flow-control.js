997e97da51f5bd7e9a3595aacbf0cabf0f2d4e7f
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-11 13:17:49 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -26,10 +26,17 @@ function ternaryTeenager(age) {
   return "You are" + (age <= 19 && age >= 13 ? " a teenager" : " not a teenager")
 }
 
-function switchAge(age) {
-  if (age <= 19 && age >= 13){
-    return "You are a teenager"
-  } else {
+function switchAge(age){
+  switch (age) {
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+    return "You are a teenager";
+  default:
     return "You have an age"
   }
 }
